1a8c2ba9d52929d8bf9d836e860248c8f31467e8
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-23 07:45:49 -0800

    Apply addApplicationStartInfoCompletionListener

--- app/src/main/java/com/tomas/repcik/appstartinfodemo/AppInfoModel.kt
+++ app/src/main/java/com/tomas/repcik/appstartinfodemo/AppInfoModel.kt
@@ -3,4 +3,4 @@ package com.tomas.repcik.appstartinfodemo
 import com.demo.core.app_start_up_info.AppStartInfoBean
 import com.tomas.repcik.appstartinfodemo.appinforepo.AppExitInfo
 
-data class AppInfoModel(val appStartUpInfoList: List<AppStartInfoBean>, val appExitInfoList: List<AppExitInfo>)
+data class AppInfoModel(val appStartUpInfoList: List<AppStartInfoBean>, val appExitInfoList: List<AppExitInfo> = emptyList()  )
