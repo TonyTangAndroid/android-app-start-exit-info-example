1d8769c478382f81fef80f7e1d0784499ece982b
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-23 06:36:42 -0800

    Add AppExitInfoEnumsMapper.kt

---
+++ app/src/main/java/com/tomas/repcik/appstartinfodemo/appinforepo/AppStartInfoModel.kt
@@ -0,0 +1,51 @@
+package com.tomas.repcik.appstartinfodemo.appinforepo
+
+enum class LaunchMode {
+  LAUNCH_MODE_SINGLE_INSTANCE,
+  LAUNCH_MODE_SINGLE_INSTANCE_PER_TASK,
+  LAUNCH_MODE_SINGLE_TASK,
+  LAUNCH_MODE_SINGLE_TOP,
+  LAUNCH_MODE_STANDARD;
+}
+
+enum class StartupState {
+  STARTUP_STATE_ERROR,
+  STARTUP_STATE_FIRST_FRAME_DRAWN,
+  STARTUP_STATE_STARTED;
+}
+
+enum class StartReason {
+  START_REASON_ALARM,
+  START_REASON_BACKUP,
+  START_REASON_BOOT_COMPLETE,
+  START_REASON_BROADCAST,
+  START_REASON_CONTENT_PROVIDER,
+  START_REASON_JOB,
+  START_REASON_LAUNCHER,
+  START_REASON_LAUNCHER_RECENTS,
+  START_REASON_OTHER,
+  START_REASON_PUSH,
+  START_REASON_SERVICE,
+  START_REASON_START_ACTIVITY;
+}
+
+enum class StartType {
+  START_TYPE_COLD,
+  START_TYPE_HOT,
+  START_TYPE_UNSET,
+  START_TYPE_WARM;
+}
+
+data class StartupTimestamps(
+  val applicationOnCreate: Long? = null,
+  val bindApplication: Long? = null,
+  val firstFrame: Long? = null,
+  val fork: Long? = null,
+  val fullyDrawn: Long? = null,
+  val initialRenderThreadFrame: Long? = null,
+  val launch: Long? = null,
+  val reservedRangeDeveloper: Long? = null,
+  val reservedRangeDeveloperStart: Long? = null,
+  val reservedRangeSystem: Long? = null,
+  val surfaceFlingerCompositionComplete: Long? = null
+)
