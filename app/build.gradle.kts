31a5a363c581173f3114e327098431da9a7d360f
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-23 08:36:22 -0800

    Revert "Bump down the compile sdk to 34 to remove the minimum code of api surface 35"
    This reverts commit 2af82c8b8cc3d4d4f70dbbfccc56a48043a10421.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
   namespace = "com.tomas.repcik.appstartinfodemo"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.tomas.repcik.appstartinfodemo"
     minSdk = 26
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
