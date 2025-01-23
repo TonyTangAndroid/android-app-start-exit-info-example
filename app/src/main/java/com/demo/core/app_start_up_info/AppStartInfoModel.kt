394f2aa313fb53969aff3e59adf6327656d02c97
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-23 06:47:00 -0800

    Add AppStartInfoMapper

--- app/src/main/java/com/demo/core/app_start_up_info/AppStartInfoModel.kt
+++ app/src/main/java/com/demo/core/app_start_up_info/AppStartInfoModel.kt
@@ -1,5 +1,16 @@
-package com.tomas.repcik.appstartinfodemo.appinforepo
+package com.demo.core.app_start_up_info
 
+import android.content.Intent
+
+data class AppStartInfo(
+  val startType: StartType, // what is the starting point, e.g. hot/cold start
+  val startupState: StartupState, // what stage of starting of the app occured, e.g. started/first image rendered
+  val startReason: StartReason, // why the app was started
+  val launchMode: LaunchMode, // e.g. SINGLE_TASK, SINGLE_TOP
+  val timestamps: StartupTimestamps, // timestamps of the events
+  val wasForceStopped: Boolean, // if the app was forced previously
+  val startIntent: Intent?
+)
 enum class LaunchMode {
   LAUNCH_MODE_SINGLE_INSTANCE,
   LAUNCH_MODE_SINGLE_INSTANCE_PER_TASK,
--- app/src/main/java/com/tomas/repcik/appstartinfodemo/appinforepo/AppInfoRepo.kt
+++ app/src/main/java/com/tomas/repcik/appstartinfodemo/appinforepo/AppInfoRepo.kt
@@ -4,6 +4,8 @@ import android.app.ActivityManager
 import android.app.ApplicationExitInfo
 import android.app.ApplicationStartInfo
 import android.content.Context
+import com.demo.core.app_start_up_info.AppStartInfo
+import com.demo.core.app_start_up_info.AppStartInfoMapper
 import java.util.concurrent.Executor
 
 
@@ -43,30 +45,11 @@ class AppInfoRepo(context: Context) {
 
 
   private fun mapStartInfo(startInfo: ApplicationStartInfo): AppStartInfo {
-    val startType = AppStartInfoModelMapper.toAppStartType(startInfo.startType)
-    val startReason = AppStartInfoModelMapper.toAppStartReason(startInfo.reason)
-    val startUpState = AppStartInfoModelMapper.toAppStartState(startInfo.startupState)
-    val launchMode = AppStartInfoModelMapper.toAppLaunchMode(startInfo.launchMode)
-    val startTimestamp = AppStartInfoModelMapper.toStartupTimestamps(startInfo.startupTimestamps)
-    val wasForceStopped = startInfo.wasForceStopped()
-    val startIntent = startInfo.intent
-    return AppStartInfo(
-      startType,
-      startUpState,
-      startReason,
-      launchMode,
-      startTimestamp,
-      wasForceStopped,
-      startIntent
-    )
+    return AppStartInfoMapper.mapStartInfo(startInfo)
   }
 
   private fun mapExitInfo(exitInfo: ApplicationExitInfo): AppExitInfo {
-    val exitReason = AppExitInfoEnumsMapper.toExitReason(exitInfo.reason)
-    val importance = AppExitInfoEnumsMapper.toProcessImportance(exitInfo.importance)
-    val description = exitInfo.description
-    val timestamp = exitInfo.timestamp
-    return AppExitInfo(exitReason, importance, description, timestamp)
+    return AppStartInfoMapper.mapExitInfo(exitInfo)
   }
 
 }
