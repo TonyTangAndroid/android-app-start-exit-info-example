a2108d45065b87dc32db7be4b7c25441c90139b2
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-23 06:52:30 -0800

    Ignore start info intent

--- app/src/main/java/com/demo/core/app_start_up_info/AppStartInfoMapper.kt
+++ app/src/main/java/com/demo/core/app_start_up_info/AppStartInfoMapper.kt
@@ -8,26 +8,28 @@ import com.tomas.repcik.appstartinfodemo.appinforepo.AppExitInfoEnumsMapper
 
 object AppStartInfoMapper {
 
-   fun mapStartInfo(startInfo: ApplicationStartInfo): AppStartInfoBean {
+  fun mapStartInfo(startInfo: ApplicationStartInfo): AppStartInfoBean {
     val startType = AppStartInfoModelMapper.toAppStartType(startInfo.startType)
     val startReason = AppStartInfoModelMapper.toAppStartReason(startInfo.reason)
     val startUpState = AppStartInfoModelMapper.toAppStartState(startInfo.startupState)
     val launchMode = AppStartInfoModelMapper.toAppLaunchMode(startInfo.launchMode)
     val startTimestamp = AppStartInfoModelMapper.toStartupTimestamps(startInfo.startupTimestamps)
     val wasForceStopped = startInfo.wasForceStopped()
-    val startIntent = startInfo.intent
+    val startIntent = startInfo.intent.toString()
+    println("Ignored app_startup intent : $startIntent")
     return AppStartInfoBean(
       startType,
       startUpState,
       startReason,
       launchMode,
       startTimestamp,
-      wasForceStopped,
-      startIntent
-    )
+      wasForceStopped
+    ).also {
+      println("Assembled app_startup bean: $it")
+    }
   }
 
-   fun mapExitInfo(exitInfo: ApplicationExitInfo): AppExitInfo {
+  fun mapExitInfo(exitInfo: ApplicationExitInfo): AppExitInfo {
     val exitReason = AppExitInfoEnumsMapper.toExitReason(exitInfo.reason)
     val importance = AppExitInfoEnumsMapper.toProcessImportance(exitInfo.importance)
     val description = exitInfo.description
