901d62a25af89d8caaf810f85e1cca5f3ead9583
Zhilu Tang <tonythompsoncmu@gmail.com>
2025-01-23 08:35:33 -0800

    Bump down the compile sdk to 34 to remove the minimum code of api surface 35

--- app/src/main/java/com/demo/core/app_start_up_info/model/AppInfoRepo.kt
+++ app/src/main/java/com/demo/core/app_start_up_info/model/AppInfoRepo.kt
@@ -1,8 +1,6 @@
 package com.demo.core.app_start_up_info.model
 
 import android.content.Context
-import android.os.Build
-import com.demo.core.app_start_up_info.ProdAppInfoRepo
 import java.util.concurrent.Executor
 
 interface AppInfoRepo {
@@ -13,11 +11,7 @@ interface AppInfoRepo {
 
   companion object {
     fun create(context: Context): AppInfoRepo {
-      return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-        ProdAppInfoRepo(context)
-      } else {
-        StubAppInfoRepo
-      }
+      return StubAppInfoRepo
     }
   }
 
